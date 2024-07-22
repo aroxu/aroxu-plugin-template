3b55e854d5336b079f24c22e8817ae93d5a61672
aroxu <30624945+aroxu@users.noreply.github.com>
2024-07-22 19:40:49 +0900

    :sparkle: Paper Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,11 @@ dependencies {
     compileOnly(libs.coroutines)
     compileOnly(libs.mccoroutines)
     compileOnly(libs.mccoroutinesCore)
+
+    paperLibrary(libs.cloud)
+    paperLibrary(libs.coroutines)
+    paperLibrary(libs.mccoroutines)
+    paperLibrary(libs.mccoroutinesCore)
 }
 
 tasks {
@@ -49,13 +54,16 @@ idea {
     }
 }
 
-bukkit {
+paper {
     name = rootProject.name
     version = rootProject.version.toString()
 
-    main = "${project.group}.${codeName}.plugin.${codeName.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }}Plugin"
+    main =
+        "${project.group}.${codeName}.plugin.${codeName.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }}Plugin"
+    loader = "${project.group}.${codeName}.plugin.loader.${codeName.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }}PluginLoader"
 
-    apiVersion = libs.paper.get().version!!.take(4)
+    apiVersion = libs.paper.get().version!!.replace("-R0.1-SNAPSHOT", "")
 
-    libraries = listOf(libs.cloud.get().toString(), libs.coroutines.get().toString(), libs.mccoroutines.get().toString(), libs.mccoroutinesCore.get().toString())
+    generateLibrariesJson = true
+    foliaSupported = false
 }
